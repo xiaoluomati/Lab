af6388eed0cbd85706cf3576199c0e59df70bb6f
xiaoluomati <1109988978@qq.com>
2019-07-15 15:39:31 +0800

    .

---
+++ src/main/java/cn/edu/nju/lims/LimsApplication.java
@@ -0,0 +1,13 @@
+package cn.edu.nju.lims;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LimsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LimsApplication.class, args);
+    }
+
+}
