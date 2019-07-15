71ab14f92c35d7903c2bd43441e64d5de5747e7b
xiaoluomati <1109988978@qq.com>
2019-07-16 01:03:43 +0800

    testall

--- src/test/java/cn/edu/nju/lims/ActivitiTest.java
+++ src/test/java/cn/edu/nju/lims/ActivitiTest.java
@@ -13,11 +13,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import javax.transaction.Transactional;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 @RunWith(SpringJUnit4ClassRunner.class)
+@Transactional
 @SpringBootTest(classes = LimsApplication.class)
 public class ActivitiTest {
 
@@ -37,17 +39,51 @@ public class ActivitiTest {
         variables.put("reportItemA", "High");
         variables.put("reportItemB", "Low");
         runtimeService.startProcessInstanceByKey("report", variables);
+        TestComposeReport();
+        TestCheckReport();
+        TestComposeReport();
+        TestPrintReport();
     }
 
-    @Test
-    public void TestComposeReport() {
+    private void TestComposeReport() {
         String assignee = "composer";
         List<Task> tasks = taskService.createTaskQuery().taskAssignee(assignee).list();
+        System.out.println(tasks.size());
+        for (Task task : tasks) {
+            String id = task.getId();
+            System.out.println("taskId:" + id +
+                    ",taskName:" + task.getName() +
+                    ",assignee:" + task.getAssignee() +
+                    ",createTime:" + task.getCreateTime());
+            System.out.println("reportName = " + taskService.getVariable(id, "reportName"));
+            taskService.complete(task.getId());
+        }
+    }
+
+    private void TestCheckReport() {
+        String assignee = "checker";
+        List<Task> tasks = taskService.createTaskQuery().taskAssignee(assignee).list();
+        System.out.println(tasks.size());
         for (Task task : tasks) {
             System.out.println("taskId:" + task.getId() +
                     ",taskName:" + task.getName() +
                     ",assignee:" + task.getAssignee() +
                     ",createTime:" + task.getCreateTime());
+            taskService.setVariable(task.getId(), "isAgree", false);
+            taskService.complete(task.getId());
+        }
+    }
+
+    private void TestPrintReport(){
+        String assignee = "user";
+        List<Task> tasks = taskService.createTaskQuery().taskAssignee(assignee).list();
+        System.out.println(tasks.size());
+        for (Task task : tasks) {
+            System.out.println("taskId:" + task.getId() +
+                    ",taskName:" + task.getName() +
+                    ",assignee:" + task.getAssignee() +
+                    ",createTime:" + task.getCreateTime());
+            taskService.complete(task.getId());
         }
     }
 
